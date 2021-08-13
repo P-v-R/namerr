9d12658b4917432487be06011506ee2636b21c16
Perryvon <Perry.j.vr@gmail.com>
2021-08-12 17:48:03 -0700

    added some basic name and email generation
    makeUsers - makeNames - makeName - makeEmails - makeEmail

---
+++ names/emailDomains.js
@@ -0,0 +1,12 @@
+let emailDomains = [
+  "gmail.com",
+  "comcast.net",
+  "yahoo.com",
+  "apple.com",
+  "microsoft.com",
+  "hotmail.com",
+  "aol.com",
+  "msn.com"
+]
+
+module.exports = {emailDomains}
